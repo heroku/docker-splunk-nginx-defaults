8164fb84a7e99eef9fe106822bf54c4ff21d5121
Josh Mervine <joshua@mervine.net>
2018-11-10 13:46:06 -0800

    tweak entrypoint

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 test -z "$DEBUG" || set -x
-#set -ue
+set -ue
 
 touch /etc/secrets.env
 
-# If /etc/secrets/secrets.yaml exists, then run summon and generate /etc/secrets.
+# If /data/secrets/secrets.yaml exists, then run summon and generate /etc/secrets.
 if test -f /data/secrets/secrets.yaml; then
   /usr/local/bin/summon -p summon-aws-secrets -f /data/secrets/secrets.yaml \
     --ignore-all cat "@SUMMONENVFILE" > /etc/secrets.env
